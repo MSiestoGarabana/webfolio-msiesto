c9473178d2a0d4b39e24f345cec90d23b52a50cb
MSiesto <miguelsiesto@gmail.com>
2023-02-26 18:03:56 +0100

    create skills comp

---
+++ src/Components.Main/Skills/Skills.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import CardHeader from '../Components.header/ComponentHeader'
+import CardSkill from './CardSkill'
+import './Skills.css'
+
+
+function Skills () {
+  return (
+    <div>
+      <CardHeader heading="CHECK OUT MY" title="SKILLS"></CardHeader>
+      <div className='skills__container'>
+         <CardSkill id="React"></CardSkill>
+         <CardSkill id="Redux"></CardSkill>
+         <CardSkill id="Gatsby"></CardSkill>
+         <CardSkill id="SASS"></CardSkill>
+         <CardSkill id="JavaScript"></CardSkill>
+         <CardSkill id="Git"></CardSkill>
+         <CardSkill id="Html5"></CardSkill>
+         <CardSkill id="Css6"></CardSkill>
+      </div>
+    </div>
+  )
+}
+ export default Skills
