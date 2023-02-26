4678d7fca43a48932612d8411946afb83a50d157
MSiesto <miguelsiesto@gmail.com>
2023-02-26 11:03:02 +0100

    updating entering point

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import "./App.css"
 import Main from './Main'
+
 function App() {
   return (
     <div className="App">
+      <div>hey</div>
      <Main/>
     </div>
   )
