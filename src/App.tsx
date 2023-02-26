cfce61ff55329c123de6ba8f0e849d90d4ea8412
MSiesto <miguelsiesto@gmail.com>
2023-02-26 18:29:35 +0100

    creating navBar, importing SVG components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import "./App.css"
 import Main from './Main'
+import NavBar from './Components.Main/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
+     <NavBar></NavBar>
      <Main/>
     </div>
   )
