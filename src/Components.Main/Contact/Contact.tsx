1bdec21fae59c259201f6fba4afb8d6812534d93
MSiesto <miguelsiesto@gmail.com>
2023-02-26 19:44:04 +0100

    updating contact and positioning content

--- src/Components.Main/Contact/Contact.tsx
+++ src/Components.Main/Contact/Contact.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import CardHeader from "../Components.header/ComponentHeader";
+import "./Contact.css";
 
 function Contact() {
-  return <div></div>;
+  return (
+    <main className="contact__main">
+      <CardHeader heading="SAY HELLO" title="CONTACT"></CardHeader>
+      <div className="contact__body"> FUTURE FORM HERE</div>
+    </main>
+  );
 }
 export default Contact;
--- src/Components.Main/Skills/Skills.tsx
+++ src/Components.Main/Skills/Skills.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import CardHeader from '../Components.header/ComponentHeader'
-import CardSkill from './CardSkill'
-import './Skills.css'
+import React from "react";
+import CardHeader from "../Components.header/ComponentHeader";
+import CardSkill from "./CardSkill";
+import "./Skills.css";
 
-
-function Skills () {
+function Skills() {
   return (
-    <div>
+    <div className="skills__body">
       <CardHeader heading="CHECK OUT MY" title="SKILLS"></CardHeader>
-      <div className='skills__container'>
-         <CardSkill id="React"></CardSkill>
-         <CardSkill id="Redux"></CardSkill>
-         <CardSkill id="Gatsby"></CardSkill>
-         <CardSkill id="SASS"></CardSkill>
-         <CardSkill id="JavaScript"></CardSkill>
-         <CardSkill id="Git"></CardSkill>
-         <CardSkill id="Html5"></CardSkill>
-         <CardSkill id="Css6"></CardSkill>
+      <div className="skills__container">
+        <CardSkill id="React"></CardSkill>
+        <CardSkill id="Redux"></CardSkill>
+        <CardSkill id="Gatsby"></CardSkill>
+        <CardSkill id="SASS"></CardSkill>
+        <CardSkill id="JavaScript"></CardSkill>
+        <CardSkill id="Git"></CardSkill>
+        <CardSkill id="Html5"></CardSkill>
+        <CardSkill id="Css6"></CardSkill>
       </div>
     </div>
-  )
+  );
 }
- export default Skills
+export default Skills;
