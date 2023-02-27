2635feed00cebdbd4af61a1ac29bf663c88d5bb3
MSiesto <miguelsiesto@gmail.com>
2023-02-27 19:55:48 +0100

    importing icons & creating getIcon( )

--- src/Components.Main/Skills/CardSkill.tsx
+++ src/Components.Main/Skills/CardSkill.tsx
@@ -1,10 +1,22 @@
+import "./CardSkill.css";
+import cssIcon from "../../images/skillIcons/css-3.png";
+import gatsbyIcon from "../../images/skillIcons/gatsby.png";
+import gitIcon from "../../images/skillIcons/git.png";
+import htmlIcon from "../../images/skillIcons/html5.png";
+import jsIcon from "../../images/skillIcons/js.png";
+import reduxIcon from "../../images/skillIcons/redux-icon.png";
+import sassIcon from "../../images/skillIcons/sass.png";
+import reactIcon from "../../images/skillIcons/science.png";
 
-import './CardSkill.css'
+function getIcon(id) {
+  console.log(id);
+  return gitIcon;
+}
 
-function CardSkill({id}) {
+function CardSkill({ id }) {
   return (
     <div className="cardSkill">
-      <img></img>
+      <img src={getIcon(id)} alt="noimage"></img>
       <p>{id}</p>
     </div>
   );
