f2dcd9586c08f8dbb609e80282874869bc7b764b
MSiesto <miguelsiesto@gmail.com>
2023-02-27 20:02:56 +0100

    creating switch statement

--- src/Components.Main/Skills/CardSkill.tsx
+++ src/Components.Main/Skills/CardSkill.tsx
@@ -9,14 +9,38 @@ import sassIcon from "../../images/skillIcons/sass.png";
 import reactIcon from "../../images/skillIcons/science.png";
 
 function getIcon(id) {
-  console.log(id);
-  return gitIcon;
+  switch (id) {
+    case "Css":
+      return cssIcon;
+      break;
+    case "Html5":
+      return htmlIcon;
+      break;
+    case "Git":
+      return gitIcon;
+      break;
+    case "JavaScript":
+      return jsIcon;
+      break;
+    case "SASS":
+      return sassIcon;
+      break;
+    case "Gatsby":
+      return gatsbyIcon;
+      break;
+    case "Redux":
+      return reduxIcon;
+      break;
+    case "React":
+      return reactIcon;
+      break;
+  }
 }
 
 function CardSkill({ id }) {
   return (
     <div className="cardSkill">
-      <img src={getIcon(id)} alt="noimage"></img>
+      <img className={id} src={getIcon(id)} alt="noimage"></img>
       <p>{id}</p>
     </div>
   );
