a3bf1820188ddb79208e1398d30aa238082cff38
MSiesto <miguelsiesto@gmail.com>
2023-02-27 18:48:55 +0100

    creating and positioning invisible togo-to´s

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,17 @@ function Main() {
   return (
     <div id="home" className="Main">
       <Header />
+      <div id="goto__aboutme" className="invisible__goto">
+        aboutme
+      </div>
       <AboutMe />
+      <div id="goto__skills" className="invisible__goto">
+        skills
+      </div>
       <Skills />
+      <div id="goto__projects" className="invisible__goto">
+        projects
+      </div>
       <Projects />
       <Contact />
       <footer className="footer" id="bottom">
