b63ae385f2bf07595e26c41e321aef066a44c0ab
MSiesto <miguelsiesto@gmail.com>
2023-02-28 18:18:44 +0100

    fixing buttons link

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./NavBar.css";
 
 function NavBar() {
@@ -6,8 +5,8 @@ function NavBar() {
     <nav className="navBar">
       <h1 className="navBar__Logo">MS</h1>
       <div className="navBar__Container--Buttons">
-        <button className="navBar__Button">
-          <a href="#goto__home">
+        <a href="#goto__home">
+          <button className="navBar__Button">
             <svg
               className="navBar__icon"
               fill="none"
@@ -19,12 +18,12 @@ function NavBar() {
             >
               <path d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
             </svg>
-          </a>
-          <p className="navBar__text">Home</p>
-        </button>
+            <p className="navBar__text">Home</p>
+          </button>
+        </a>
 
-        <button className="navBar__Button">
-          <a href="#goto__aboutme">
+        <a href="#goto__aboutme">
+          <button className="navBar__Button">
             <svg
               className="navBar__icon"
               fill="none"
@@ -36,12 +35,12 @@ function NavBar() {
             >
               <path d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
             </svg>
-          </a>
-          <p className="navBar__text">About</p>
-        </button>
+            <p className="navBar__text">About</p>
+          </button>
+        </a>
 
-        <button className="navBar__Button">
-          <a href="#goto__skills">
+        <a href="#goto__skills">
+          <button className="navBar__Button">
             <svg
               className="navBar__icon"
               fill="none"
@@ -53,12 +52,12 @@ function NavBar() {
             >
               <path d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"></path>
             </svg>
-          </a>
-          <p className="navBar__text">Skills</p>
-        </button>
+            <p className="navBar__text">Skills</p>
+          </button>
+        </a>
 
-        <button className="navBar__Button">
-          <a href="#goto__projects">
+        <a href="#goto__projects">
+          <button className="navBar__Button">
             <svg
               className="navBar__icon"
               fill="none"
@@ -70,12 +69,12 @@ function NavBar() {
             >
               <path d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"></path>
             </svg>
-          </a>
-          <p className="navBar__text">Projects</p>
-        </button>
+            <p className="navBar__text">Projects</p>
+          </button>
+        </a>
 
-        <button className="navBar__Button">
-          <a href="#goto__contact">
+        <a href="#goto__contact">
+          <button className="navBar__Button">
             <svg
               className="navBar__icon"
               fill="none"
@@ -87,9 +86,9 @@ function NavBar() {
             >
               <path d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"></path>
             </svg>
-          </a>
-          <p className="navBar__text">Contact</p>
-        </button>
+            <p className="navBar__text">Contact</p>
+          </button>
+        </a>
       </div>
     </nav>
   );
