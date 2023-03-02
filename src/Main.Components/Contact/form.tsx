edb059324fc083525430923c1ed111ecef6e9146
MSiesto <miguelsiesto@gmail.com>
2023-03-02 19:55:04 +0100

    Creating new form inputs & styling it

--- src/Main.Components/Contact/form.tsx
+++ src/Main.Components/Contact/form.tsx
@@ -1,18 +1,48 @@
 import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
+import "./form.css";
 function ContactForm() {
   const [state, handleSubmit] = useForm("mknabzdq");
+  console.log(state);
   if (state.succeeded) {
     return <p>Thanks for joining!</p>;
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input id="email" type="email" name="email" />
+    <form onSubmit={handleSubmit} className="form">
+      <input
+        id="name"
+        type="name"
+        placeholder="Your Name*"
+        name="name"
+        className="nameInput"
+      />
+      <input
+        id="email"
+        type="email"
+        placeholder="Your Email*"
+        name="email"
+        className="emailInput"
+      />
+      <input
+        id="subject"
+        type="subject"
+        placeholder="Subject*"
+        name="subject"
+        className="subjectInput"
+      />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <textarea id="message" name="message" />
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Your Message*"
+        className="messageInput"
+      />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button type="submit" disabled={state.submitting}>
+      <button
+        className="submitButton"
+        type="submit"
+        disabled={state.submitting}
+      >
         Submit
       </button>
     </form>
