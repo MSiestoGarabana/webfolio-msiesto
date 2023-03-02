221339fada1c2a73cb8401fb13b9d0c89df098cd
MSiesto <miguelsiesto@gmail.com>
2023-03-02 18:36:11 +0100

    implementing first form version

--- src/Main.Components/Contact/Contact.tsx
+++ src/Main.Components/Contact/Contact.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import CardHeader from "../Components.header/ComponentHeader";
 import "./Contact.css";
+import Form from "./form";
 
 function Contact() {
   return (
     <main id="goto__contact" className="contact__main">
       <CardHeader heading="SAY HELLO" title="CONTACT"></CardHeader>
-      <div className="contact__body"> FUTURE FORM HERE</div>
+      <div className="contact__body">
+        {" "}
+        FUTURE FORM HERE
+        <Form />
+      </div>
     </main>
   );
 }
