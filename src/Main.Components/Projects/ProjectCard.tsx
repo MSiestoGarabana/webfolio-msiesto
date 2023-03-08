6cb139fc7c3e761e2f29d6cce236364764ee20fd
MSiesto <miguelsiesto@gmail.com>
2023-03-08 14:10:21 +0100

    re-structuring ProjectCard

--- src/Main.Components/Projects/ProjectCard.tsx
+++ src/Main.Components/Projects/ProjectCard.tsx
@@ -1,31 +1,23 @@
+import "./ProjectCard.css";
 export default function ProjectCard({ ...data }) {
   return (
     <div className="projects__card" id={data.id}>
-      <div className="IMG__container--projectCard">
-        <img
-          src={data.img}
-          className="projects__image"
-          alt="no image found"
-        ></img>
-      </div>
-      <div className="projects__card--text">
-        <div className="text__titleContainer">
-          <h4>{data.title}</h4>
-          <div>
-            <h5>Technologies used</h5>
-            {data.technologies.map((technology) => (
-              <p>{technology}</p>
-            ))}
-          </div>
+      <img src={data.img} className="projects__image" alt="no image found" />
+
+      <div className="Card__container--text">
+        <h4>{data.title}</h4>
+        <div className="Card__container--technologies">
+          <h5>Technologies used:</h5>
+          {data.technologies.map((technology) => (
+            <p>{technology}</p>
+          ))}
         </div>
-        <div className="text__ulcontainer">
-          <ul>
-            {data.text.map((text) => (
-              <li>{text}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="links__container">
+        <ul>
+          {data.text.map((text) => (
+            <li>{text}</li>
+          ))}
+        </ul>
+        <div className="Card__container--links">
           <a href={data.liveLink} target="blank">
             Live
           </a>
