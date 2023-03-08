15c241d1dad1cdc25ef60c7aa895012cb99851c3
MSiesto <miguelsiesto@gmail.com>
2023-03-08 17:40:04 +0100

    styling projectCard

--- src/Main.Components/Projects/ProjectCard.tsx
+++ src/Main.Components/Projects/ProjectCard.tsx
@@ -1,27 +1,27 @@
 import "./ProjectCard.css";
 export default function ProjectCard({ ...data }) {
   return (
-    <div className="projects__card" id={data.id}>
-      <img src={data.img} className="projects__image" alt="no image found" />
+    <div className="Card" id={data.id}>
+      <img src={data.img} className="Card__image" alt="no image found" />
 
       <div className="Card__container--text">
-        <h4>{data.title}</h4>
+        <h4 className="Card__text--title">{data.title}</h4>
         <div className="Card__container--technologies">
-          <h5>Technologies used:</h5>
+          <h4 className="Card__text--techTitle">Technologies used:</h4>
           {data.technologies.map((technology) => (
-            <p>{technology}</p>
+            <p className="Card__text--technology">{technology}</p>
           ))}
         </div>
-        <ul>
+        <ul className="Card__unorderedlist--list">
           {data.text.map((text) => (
-            <li>{text}</li>
+            <li className="Card__unorderedlist--text">{text}</li>
           ))}
         </ul>
         <div className="Card__container--links">
-          <a href={data.liveLink} target="blank">
+          <a className="Card__link" href={data.liveLink} target="blank">
             Live
           </a>
-          <a href={data.repoLink} target="blank">
+          <a className="Card__link" href={data.repoLink} target="blank">
             Repository
           </a>
         </div>
